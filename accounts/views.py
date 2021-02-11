4623d01ad25ce1991ec8d7bfad9a35208763e980
kei-1011 <k_kojima0407@yahoo.co.jp>
2021-02-11 17:41:44 +0900

    signup

--- accounts/views.py
+++ accounts/views.py
@@ -11,3 +11,6 @@ class LogoutView(views.LogoutView):
     if self.request.user.is_authenticated:
       self.logout()
       return redirect('/')
+
+class SignupView(views.SignupView):
+  template_name = 'accounts/signup.html'
